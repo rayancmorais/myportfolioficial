a03e75c685a74e27206c69e6aede87a81b1c697b
Rayan Morais <rayan_cm2021@icloud.com>
2025-08-07 15:47:35 -0300

    changes intro font-siz

--- src/components/pageSections/introSection/IntroSection.styles.js
+++ src/components/pageSections/introSection/IntroSection.styles.js
@@ -36,14 +36,13 @@ export const IntroSectionBriefing2 = styled.h2`
   font-size: 56px;
   margin-top: 10px;
   font-weight: 600;
-  font-family: Inconsolata;
   @media (max-width: 768px) {
     font-size: 32px;
   }
 `;
 
 export const IntroSectionBriefing3 = styled.h6`
-  font-family: Inconsolata;
+  font-family: Maven Pro;
   color: #878ea1;
   font-size: 20px;
   margin-top: 20px;
